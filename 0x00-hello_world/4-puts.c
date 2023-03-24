300c4cf65100b6fd2656bd46ecaed984de854240
Njigwa <wambuguderrick60@gmail.com>
2023-03-24 10:55:18 -0700

    to print a script followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - prints a string of characters with a new line
+ * main - A program that  prints a string of characters followed by  a new line
  * return 0 (success)
  */
 int main (void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
