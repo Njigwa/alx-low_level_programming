def5d2d014018a274361bc2580a180420c94a283
Njigwa <wambuguderrick60@gmail.com>
2023-03-24 10:48:43 -0700

    to print a script followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - prints a string of characters with a new line
  * return 0 (success)
  */
-int main(void)
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts (\"Programming is like building a multilingual puzzle");
+	return (0);
 }
