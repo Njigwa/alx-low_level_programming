bc300e97fb9af0ee8421d6336850a2076a84a37b
Njigwa <wambuguderrick60@gmail.com>
2023-03-24 11:21:44 -0700

    to print a string of characters exactly as is

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main - prints exactly the string below using pritf function
+ * main - A program prints exactly the string using pritf function
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar,but the outcome is a piece of art,");
 	return (0);
 }
