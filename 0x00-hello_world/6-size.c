c6633c7ab5d50824c4b5965e4fa8ebaa2d41cea4
Njigwa <wambuguderrick60@gmail.com>
2023-03-25 01:38:03 -0700

    to print various data types and their sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,9 @@ int main(void)
 	int i;
 	double d;
 	char c;
-	long int b;
-	long long int e;
-	float f;
-
-	printf("The size of an integer is: %lu bytes(%)\n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu bytes(%)\n", (unsigned long)sizeof(d));
-	printf("The size of a character is: %lu bytes(%)\n", (unsigned long)sizeof(c));
-	printf("The size of a long int is: %lu bytes(%)\n", (unsigned long)sizeof(b));
-	printf("The size of a long long int is: %lu bytes(%)\n", (unsigned long)sizeof(e));
-	printf("The size of a float is: %lu bytes(%)\n", (unsigned long)sizeof(f));
+	
+	printf("The size of an integer is:lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of a character is: %lu.\n", (unsigned long)sizeof(c));
 	return (0)
 }
