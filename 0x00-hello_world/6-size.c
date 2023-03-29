27de26adf7025aa11fe24e3b603923bf90a7964b
Njigwa <wambuguderrick60@gmail.com>
2023-03-29 07:48:04 -0700

    A program that prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
- * main - A program that prints a size of various data types 
+ * main - A program that prints a size of various data types
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	
-	printf("The size of a char is: %lu\n byte(s)", sizeof(char));
-	printf("The size of an int is: %lu\n byte(s)", sizeof(int));
-	printf("The size of a long int is: %lu\n byte(s)", sizeof(long int));
-	printf("The size of a long long int is: %lu\n byte(s)", sizeof(long long int));
-	printf("The size of a float is: %lu\n byte(s)", sizeof(float));
-	
+	printf("Size of a char is: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int is: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int is: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float is: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
