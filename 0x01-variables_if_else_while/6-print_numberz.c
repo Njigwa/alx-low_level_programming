eeb26a05527c207d41c058fc22792006fac88fdd
Njigwa <wambuguderrick60@gmail.com>
2023-03-29 12:28:58 -0700

    A program that prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digit numbers of base ten
+ *
+ *Return: 0 (Succes)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 0; x++)
+		putchar(x + '0');
+	putchar('\n');
+	return (0);
+}
